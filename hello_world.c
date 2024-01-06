17390f21e923e63596a67ad114e60fd90f1b37ef
Rahul_R_S01 <rahul.r@thinkpalm.com>
2024-01-07 02:00:29 +0530

    hello world print on dmesg on loading and goodbye on unloading

---
+++ hello_world.c
@@ -0,0 +1,26 @@
+#include <linux/module.h>     /* Needed by all modules */
+#include <linux/kernel.h>     /* Needed for KERN_INFO */
+#include <linux/init.h>       /* Needed for the macros */
+
+///< The license type -- this affects runtime behavior
+MODULE_LICENSE("GPL");
+///< The author -- visible when you use modinfo
+MODULE_AUTHOR("Rahul RS");
+///< The description -- see modinfo
+MODULE_DESCRIPTION("Hello World");
+///< The version of the module
+MODULE_VERSION("0.1");
+
+static int __init hello_start(void)
+{
+    printk(KERN_INFO "Hello world\n");
+    return 0;
+}
+
+static void __exit hello_end(void)
+{
+    printk(KERN_INFO "Goodbye\n");
+}
+
+module_init(hello_start);
+module_exit(hello_end);
