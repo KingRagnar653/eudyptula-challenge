edbdc604d40867cf17218e68daa3d31dda513bf9
Rahul_R_S01 <rahul.r@thinkpalm.com>
2024-01-07 14:31:23 +0530

    added .clang-format file to resemble linux coding style to some extent

--- hello_world.c
+++ hello_world.c
@@ -1,6 +1,6 @@
-#include <linux/module.h>     /* Needed by all modules */
-#include <linux/kernel.h>     /* Needed for KERN_INFO */
-#include <linux/init.h>       /* Needed for the macros */
+#include <linux/init.h>   /* Needed for the macros */
+#include <linux/kernel.h> /* Needed for KERN_INFO */
+#include <linux/module.h> /* Needed by all modules */
 
 ///< The license type -- this affects runtime behavior
 MODULE_LICENSE("GPL");
@@ -13,14 +13,11 @@ MODULE_VERSION("0.1");
 
 static int __init hello_start(void)
 {
-    printk(KERN_INFO "Hello world\n");
-    return 0;
+	printk(KERN_INFO "Hello world\n");
+	return 0;
 }
 
-static void __exit hello_end(void)
-{
-    printk(KERN_INFO "Goodbye\n");
-}
+static void __exit hello_end(void) { printk(KERN_INFO "Goodbye\n"); }
 
 module_init(hello_start);
 module_exit(hello_end);
